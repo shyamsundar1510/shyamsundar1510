74798ddcb3f29efd11aee23652ecf3464717aae7
shyamsundar1510 <shyamsundar1510@gmail.com>
2021-07-15 16:35:27 +0530

    Moving to Git

---
+++ Scala.practice/src/shyam.scala
@@ -0,0 +1,25 @@
+import java.io.FileInputStream
+
+object shyam {
+  def main (args:Array[String])=
+  {
+    
+    val a=10
+    val b=12
+    val c=a+b
+    println(c)
+    
+    def add(a:Int,b:Int)={a+b}
+    
+    val cx=add(2,3)
+    println(cx)
+    
+    class division {
+      def data(a:Int,b:Int)=a/b
+    }
+    
+    val sss=new division
+    val date=sss.data(10, 5)
+    println(date)
+}
+}
